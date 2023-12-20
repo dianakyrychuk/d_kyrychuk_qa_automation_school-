834df2cfd3095c26690d456d4c587518ee0fe04d
Diana Kyrychuk <diana.kyrychuk@quarks.tech>
2023-12-20 17:15:59 +0200

    homework 8

---
+++ src/main/java/Lesson8/Main.java
@@ -0,0 +1,23 @@
+package Lesson8;
+
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
+    public void myHomework() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/Users/dianakyrychuk/IdeaProjects/d_kyrychuk_qa_automation_school-/src/main/java/Lesson8/test_file_for_homework"));
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occurred while reading from the file", e);
+        }
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.myHomework();
+    }
+}
+
