62fbadcc174ebf7c1ddf37b37c358ad9d29f3d32
Diana Kyrychuk <diana.kyrychuk@quarks.tech>
2024-02-01 16:37:15 +0200

    added fixes to homework 13

--- src/main/java/page/CheckoutPage.java
+++ src/main/java/page/CheckoutPage.java
@@ -2,21 +2,33 @@ package page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import java.util.Locale;
+
 import static com.codeborne.selenide.Selenide.$;
 
 
 public class CheckoutPage {
 
     private final SelenideElement finishButton = $("#finish");
+    private final SelenideElement TAX_LABEL = $(".summary_tax_label");
+    private final SelenideElement TOTAL_LABEL = $(".summary_total_label");
 
-    public CheckoutPage getTaxesPercent() {
-        System.out.println("Total amount of taxes is 8%");
-        return this;
+    public void calculateAndPrintTaxPercentage() {
+        double taxValue = extractValueFromLabel(TAX_LABEL);
+        double totalValue = extractValueFromLabel(TOTAL_LABEL);
+        double taxPercentage = (taxValue / totalValue) * 100;
+
+        String formattedTaxPercentage = String.format(Locale.US, "%.2f", taxPercentage);
+        System.out.println("Tax Percentage: " + formattedTaxPercentage + "%");
     }
 
-    public CheckoutPage clickFinish() {
+    private double extractValueFromLabel(SelenideElement label) {
+        String labelText = label.getText().replaceAll("[^\\d.]", "");
+        return Double.parseDouble(labelText);
+    }
+
+    public void clickFinish() {
         finishButton.click();
-        return this;
     }
 
 }
